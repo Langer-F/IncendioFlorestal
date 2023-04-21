d2ebf07b59b541176949e461cb13875ced3955d5
Langer-F <langerfelipe2000@gmail.com>
2023-04-21 17:24:14 -0300

    adicionei alguns KeyListener para pausar, iniciar e avançar de estagio

--- IncendioUsandoNumpy.py
+++ IncendioUsandoNumpy.py
@@ -1,17 +1,20 @@
 import numpy as np
 import random
 import matplotlib.pyplot as plt
-import time
+from pynput import keyboard
 from matplotlib.colors import ListedColormap
 
 
-TAMANHO_FLORESTA = 50
-CHANCE_ARVORE_PEGAR_FOGO = 0.1
-CHANCE_ARVORE_NASCER = 0.05
-DURACAO_PADRAO_FOGO = 3
-NUMERO_ESTAGIOS = 100
 
-#Este codigo só funciona para florestas quadradas.
+TAMANHO_FLORESTA = 200  #A floresta será uma matriz quadrada de lado TAMANHO_FLORESTA
+CHUVA = True            #CHUVA influencia fatores como espalahamento do fogo, além de adicionar uma chance aleatória de o fogo apagar espontaneamente
+
+CHANCE_ARVORE_PEGAR_FOGO = 0.1 #representa a chance de uma ARVORE virar FOGO. Cada vizinho FOGO aumenta a chance
+CHANCE_ARVORE_NASCER = 0.01     #representa a chance de uma CINZA virar ARVORE. Cada vizinho ARVORE aumenta a chance
+
+DURACAO_PADRAO_FOGO = 4     #quantas geracoes dura FOGO antes de virar CINZA
+
+
 class CINZAS():
     """Classe que representa as cinzas"""
     n = 0   #valor utilizado no color map
@@ -41,7 +44,7 @@ class FOGO():
         if (self.estagio == self.duracao):
             return True
         return False
-    
+
 
 N_ESTADOS = 6
 
@@ -100,14 +103,13 @@ def colocaFogoAleatorio(floresta,n):
         while ((isinstance(floresta[linha][coluna],ARVORE))==False):
             linha = random.randint(0,t)
             coluna = random.randint(0,t)
-        type(floresta[linha][coluna] ,FOGO)
+        floresta[linha][coluna] = FOGO()
     return floresta
         
-def calculaProximaGeracao(floresta):
+def calculaProximaGeracaoQueimada(floresta):
     """Dada uma floresta, calcula a próxima geração de acordo com as regras:
     Se uma arvore tem algum vizinho fogo, tem uma chance de ela virar fogo
-    O fogo avança para o proximo estagio, e caso ja esteja no ultimo, ele vira cinzas
-    Uma celula cinza com vizinho arvore pode virar uma arvore"""
+    O fogo avança para o proximo estagio, e caso ja esteja no ultimo, ele vira cinzas"""
     copia = floresta.copy()
     n = len(floresta)
     for i in range(n):
@@ -117,13 +119,29 @@ def calculaProximaGeracao(floresta):
                 r = random.random()
                 if (1-CHANCE_ARVORE_PEGAR_FOGO)**x < r:
                     copia[i][j] = FOGO()
-            elif isinstance(floresta[i][j],AGUA) or (isinstance(floresta[i][j], PEDRA)):
-                pass
             elif isinstance(floresta[i][j],FOGO):
                 copia[i][j].passa_proximo_estagio()
                 if (copia[i][j].ultimo_estagio()):
                     copia[i][j] = CINZAS()
             
+    return copia
+
+def calculaProximaGeracaoReflorestamento(floresta):
+    """dada uma floresta, calcula a proxima geração
+    uma cinza que tem vizinhos arvores pode virar uma arvore"""
+    copia = floresta.copy()
+    n = len(floresta)
+    for i in range(n):
+        for j in range(n):
+            if isinstance(floresta[i][j],CINZAS):
+                x = contaVizinhosARVORE(floresta,i,j)
+                r = random.random()
+                if (1-CHANCE_ARVORE_NASCER)**x < r:
+                    copia[i][j] = ARVORE()
+            elif isinstance(floresta[i][j],FOGO):
+                copia[i][j].passa_proximo_estagio()
+                if (copia[i][j].ultimo_estagio()):
+                    copia[i][j] = CINZAS()
     return copia
 
 def matriz_de_cores(floresta):
@@ -145,21 +163,73 @@ def cria_floresta(n):
     return floresta
 
 def main():
-    floresta = cria_floresta(TAMANHO_FLORESTA)
+    global PAUSED
+    global NEXT_ESTAGIO
+    global INICIAR
+    listener.start()
     
-    floresta[9] = np.full(TAMANHO_FLORESTA,AGUA())
-    floresta[TAMANHO_FLORESTA//2][TAMANHO_FLORESTA//2] = FOGO()
-    fig,ax = plt.subplots()
+    floresta = cria_floresta(TAMANHO_FLORESTA)
+    #floresta[9] = np.full(TAMANHO_FLORESTA,AGUA())                 # cria um rio na linha 9
+    #floresta[TAMANHO_FLORESTA//2][TAMANHO_FLORESTA//2] = FOGO()    #Adiciona uma celula em fogo no meio da floresta
+    floresta = colocaFogoAleatorio(floresta, 5)
+    fig,ax = plt.subplots(figsize = (25,25),layout = 'constrained') #tamanho do grafico
+    ax.set_title("Estado Inicial")
     im =ax.imshow(matriz_de_cores(floresta),cmap=CMAP,vmin = 0, vmax = N_ESTADOS-1)
+    plt.text(-100,1,"P para Pausar",fontsize = 18,ha='left', wrap = True)
+    plt.text(-100,6,"N para Proxima Etapa",fontsize = 18,ha='left', wrap = True)
+    plt.text(-100,11,"I para iniciar",fontsize = 18,ha='left', wrap = True)
     plt.show(block = False)
     plt.pause(1)
-
-    for i in range(NUMERO_ESTAGIOS):
-        floresta = calculaProximaGeracao(floresta)
-        im.set_data(matriz_de_cores(floresta))
-        plt.draw()
-        plt.pause(.5)   
     
+    while True:
+        if INICIAR:
+            break
+    i=0
+    while True:
+        if not PAUSED:
+            i = i+1
+            floresta = calculaProximaGeracaoQueimada(floresta)
+            im.set_data(matriz_de_cores(floresta))
+            ax.set_title(f"Geração {i} Estagio Queimada")
+            plt.draw()
+            plt.pause(.01)
+        if NEXT_ESTAGIO:
+            NEXT_ESTAGIO = False
+            break
+    i = 0
+    while True:
+        if not PAUSED:
+            i = i+1
+            floresta = calculaProximaGeracaoReflorestamento(floresta)
+            im.set_data(matriz_de_cores(floresta))
+            ax.set_title(f"Geracao {i} Reflorestamento")
+            plt.draw()
+            plt.pause(.01)
+        if NEXT_ESTAGIO:
+            break
+    
+#Adicionando Keylistener para facilitar o controle do programa
+def on_press(key):
+    global PAUSED
+    global NEXT_ESTAGIO
+    global INICIAR
+    try:
+        if key.char=='p':
+            PAUSED = not PAUSED
+        elif key.char == 'n':
+            NEXT_ESTAGIO = True
+        elif key.char == 'i':
+            INICIAR = True
+    except:
+        pass
+
+def on_release(key):
+    pass
+
+listener = keyboard.Listener(on_press=on_press)
+PAUSED = False
+NEXT_ESTAGIO = False
+INICIAR = False
 
 if __name__ == '__main__':
     main()
